74910a9cf92b3781610eeb9b9efc8c0095ac131c
Andri <andrikurniawanm.12@gmail.com>
2025-06-28 22:49:51 +0800

    Create UTSDDPSOAL2.java

---
+++ UTSDDPSOAL2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class soal2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int a = in.nextInt();
+        if (a > 0) {
+            System.out.println("Angka positif");
+        } else if (a < 0) {
+            System.out.println("Angka negatif");
+
+        } else {
+            System.out.println("Nol");
+        }
+    }
+}
